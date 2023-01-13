c91ce9d8668c7b3f704aa49d162ad404291c6b17
David McGuffey <dmcguffey94@gmail.com>
2023-01-12 22:31:20 -0600

    Exercise one

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Diagnostics;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a random favorite number
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            //Prompt user to guess a number
+            Console.WriteLine("Guess my favorite number.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber) //guess is too low
+            {
+                Console.WriteLine("too low.");
+            }
+            else if (userInput > favNumber) //guess is too high
+            {
+                Console.WriteLine("too high");
+            }
+            else //guess is correct
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
